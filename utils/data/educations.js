2a371434f059004603ab2948afc9a16090d1005c
Rashvika Ponduri <rashvikaponduri26@gmail.com>
2026-03-31 17:05:04 -0400

    Updated education

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -2,13 +2,13 @@ export const educations = [
   {
     id: 1,
     title: "Master's Degree - CS",
-    duration: "Jan 2019 - May 2020",
+    //duration: "Jan 2019 - May 2020",
     institution: "Kent State University",
   },
   {
     id: 2,
     title: "Bachelor's Degree - CSE",
-    duration: "Jun 2012 - Apr 2016",
+    //duration: "Jun 2012 - Apr 2016",
     institution: "Gitam University and Research",
   }
 ]
