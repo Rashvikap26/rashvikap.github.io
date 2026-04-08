6a0059c21f9f54b7a5030358ac5e46e2099c637e
Rashvika Ponduri <rashvikaponduri26@gmail.com>
2026-04-08 13:52:22 -0400

    Updated data

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,19 +1,19 @@
 export const experiences = [
   {
     id: 1,
-    title: 'Senior FullStack Developer',
+    title: 'Senior Full Stack Java Developer',
     company: "JP Morgan Chase",
     duration: "(Jan 2024 - Present)"
   },
   {
     id: 2,
-    title: "Senior FullStack Developer",
+    title: "Senior Full Stack Java Developer",
     company: "Express Scripts",
     duration: "(May 2021 - Dec 2023)"
   },
   {
     id: 3,
-    title: "FullStack Developer",
+    title: "Full Stack Java Developer",
     company: "Cricket wireless",
     duration: "(Jul 2019 - Apr 2021)"
   },
